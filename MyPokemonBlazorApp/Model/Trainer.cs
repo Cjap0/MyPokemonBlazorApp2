88c04bcbfe9e262e25bbaa9f973ae1a6cea7c428
Cjap0 <connorphoenixcoco01@gmail.com>
2024-03-27 10:32:25 +0000

    Last before...

--- MyPokemonBlazorApp/Model/Trainer.cs
+++ MyPokemonBlazorApp/Model/Trainer.cs
@@ -5,7 +5,9 @@ namespace MyPokemonBlazorApp.Model
     public class Trainer
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+         [Required]
         public Pokemon Companion { get; set; }
     }
 }
